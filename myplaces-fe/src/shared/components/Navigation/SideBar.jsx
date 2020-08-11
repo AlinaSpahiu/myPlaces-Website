e8f567a8c8a61443362dbff8434962dffe0a1f17
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-08-11 22:43:17 +0200

    Create SideBar.jsx

---
+++ myplaces-fe/src/shared/components/Navigation/SideBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {CSSTransition} from 'react-transition-group'
+
+import "./styles/SideBar.css";
+
+const SideBar = props => {
+
+    const content = <CSSTransition in={props.show} timeout={50000} classNames="slide-in-left"  mountOnEnter unmountOnExit> 
+        <aside className="side-drawer" onClick={props.onClick}> {props.children} </aside>
+        </CSSTransition> 
+    return ReactDOM.createPortal(content, document.getElementById('drawer-hook'));
+}
+
+export default SideBar;
