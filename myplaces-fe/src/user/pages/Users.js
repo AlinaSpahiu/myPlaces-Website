426ea83fdba112f54fcaa536a8023b88fc3b58c3
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-08-12 21:52:36 +0200

    Create Users.js

---
+++ myplaces-fe/src/user/pages/Users.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import UsersList from "../components/UsersList"
+
+const Users = () =>{
+    const USERS = [
+        {
+            id: 'u1',
+            image: "https://ca.slack-edge.com/TJNQP8XCG-UUX39CPPS-9bffb2a085c7-512",
+            name: "Alina Spahiu",
+            placeCount:2
+        },
+        {
+            id: 'u2',
+            image: "https://ca.slack-edge.com/TJNQP8XCG-UJTEDK7BQ-g6a7a9aa35d0-512",
+            name: "Riccardo Gulin",
+            placeCount: 1
+        },
+        {
+            id: '3',
+            image: "https://ca.slack-edge.com/TJNQP8XCG-UJF5YCSTV-9c84c1b89d6a-512",
+            name: "Stefano Casasola",
+            placeCount: 0 
+        }, {
+            id: '3',
+            image: "https://ca.slack-edge.com/TJNQP8XCG-UJF5YCSTV-9c84c1b89d6a-512",
+            name: "Stefano Casasola",
+            placeCount: 0 
+        }
+    ];
+  return(
+    
+   <UsersList items={USERS}/>
+  
+  
+    )}
+export default Users
