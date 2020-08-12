1f9a319e11f7ede5516ef309f487a98dea9eab67
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-08-12 21:53:05 +0200

    Update MainHeader.jsx

--- myplaces-fe/src/shared/components/Navigation/MainHeader.jsx
+++ myplaces-fe/src/shared/components/Navigation/MainHeader.jsx
@@ -1,12 +1,9 @@
-import React from "react";
+import React from 'react';
 
-import "./styles/MainHeader.css";
+import './styles/MainHeader.css';
 
 const MainHeader = props => {
-    return( 
-      <header className="main-header">
-        {props.children}
-      </header>
-    )};
+  return <header className="main-header">{props.children}</header>;
+};
 
-export default MainHeader
+export default MainHeader;
