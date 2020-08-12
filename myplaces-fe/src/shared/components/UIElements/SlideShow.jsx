c5f42fb38445ee8b96fdd9fe2ac3f545fda6e758
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-08-12 21:52:57 +0200

    Create SlideShow.jsx

---
+++ myplaces-fe/src/shared/components/UIElements/SlideShow.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+
+import "./styles/SlideShow.css"
+
+
+const SlideShow = () => {
+  return (<>
+  <div class='roller'>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+  <div class='slice'></div>
+</div>
+</>
+   )}
+export default SlideShow
