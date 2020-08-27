9731b9b2341327865017141ae9a163dbe45d7f7c
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-08-27 15:19:24 +0200

    Create placeModel.js

---
+++ myplaces-be/routes/places/placeModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const User = require('../users/userModel')
+
+const Schema = mongoose.Schema;
+
+const placeSchema = new Schema ({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+    address: { type: String, required: true },
+    // location: {
+    //            lat: {type: Number, required: true},
+    //            lng: { type: Number, required: true}
+    //           },
+    creator: { type: mongoose.Types.ObjectId, required: true, ref: 'User' } // makes the connection between users and places
+});
+
+module.exports = mongoose.model('Place', placeSchema);
