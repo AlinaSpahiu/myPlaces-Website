5e714edddb5b8d8ed5a8151c1e52f06fb25f396a
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-09-05 22:54:02 +0200

    Update userModel.js

--- myplaces-be/routes/users/userModel.js
+++ myplaces-be/routes/users/userModel.js
@@ -7,7 +7,7 @@ const userSchema = new Schema({
     email: {type: String, require: true, unique: true},
     password: {type: String, required: true, minlength: 6},
     image: {type: String, required: true},
-    places: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Place' }],
+    places: [{ type: mongoose.Types.ObjectId, ref: 'Place' }],
 });
 
 userSchema.plugin(uniqueValidator);
